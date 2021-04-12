d90bcdcd47989d59bf000508f21daea7e1fc5830
lucasbayol <78857505+lucasbayol@users.noreply.github.com>
2021-04-12 10:19:51 -0400

    Add files via upload

---
+++ BasicPortScan.ps1
@@ -0,0 +1,18 @@
+﻿$ip = Read-Host "Host | IP "
+$portlist = Read-Host "Quel ports souhaitez-vous vérifier séparer par virgule "
+foreach ($port in $portlist.split(',')){ 
+
+$scan = Test-NetConnection $ip -Port $port | Select-Object TcpTestSucceeded <# Full TCP on said port #>
+if ( $scan )  <# no need to write equal true in PS #>
+{
+Write-Host "Le port $port est ouvert"  <# echo equivalent in PS #>
+} else {
+Write-Host "Le port $port est fermé"
+}
+
+}
+
+
+
+
+
