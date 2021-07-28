2ab00744633dc466f80018e09f9d49dc7c5c1beb
lucasbayol <78857505+lucasbayol@users.noreply.github.com>
2021-07-28 11:13:58 -0400

    Add files via upload
    This script displays potentially insecure/kerberoastable service accounts.

---
+++ DomainName_inServiceAccount.ps1
@@ -0,0 +1,6 @@
+﻿$serviceaccount = Get-WmiObject win32_service | Select-Object -property Name, Startname
+for ($i = 0; $i -le $serviceaccount.count; $i++){
+if ($serviceaccount[$i] -match "$env:USERDOMAIN" ){
+$serviceaccount[$i]
+}
+}
